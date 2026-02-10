e4f968b89c5b52fb52e6751267edc7ab7f32f3ca
adit <raditya.ikhlas@ui.ac.id>
2026-02-10 13:04:22 +0700

    html templates

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/controller/ ProductController.java
@@ -0,0 +1,26 @@
+@Controller
+@RequestMapping("/product")
+public class ProductController {
+    @Autowired
+    private ProductService service;
+
+    @GetMapping("/create")
+    public String createProductPage(Model model) {
+        Product product = new Product();
+        model.addAttribute("product", product);
+        return "createProduct";
+    }
+
+    @PostMapping("/create")
+    public String createProductPost(@ModelAttribute Product product, Model model) {
+        service.create(product);
+        return "redirect:list";
+    }
+
+    @GetMapping("/list")
+    public String productListPage(Model model) {
+        List<Product> allProducts = service.findAll();
+        model.addAttribute("products", allProducts);
+        return "productList";
+    }
+}
---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductServiceImpl.java
@@ -0,0 +1,19 @@
+@Service
+public class ProductServiceImpl implements ProductService {
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Override
+    public Product create(Product product) {
+        productRepository.create(product);
+        return product;
+    }
+
+    @Override
+    public List<Product> findAll() {
+        Iterator<Product> productIterator = productRepository.findAll();
+        List<Product> allProduct = new ArrayList<>();
+        productIterator.forEachRemaining(allProduct::add);
+        return allProduct;
+    }
+}
